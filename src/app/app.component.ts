49e91fc7701486f4bc2be8e448b2cd7071908ad8
timofiidev <timofiikononyhin@gmail.com>
2020-09-02 23:51:28 -0700

    testing

---
+++ src/app/app.component.ts
@@ -0,0 +1,34 @@
+import { enableProdMode } from '@angular/core';
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import * as Sentry from '@sentry/angular';
+import { Integrations } from "@sentry/tracing";
+
+import { AppModule } from './app/app.module';
+
+Sentry.init({
+  dsn: 'https://9d2f0bb96e8b48439036e0dee3c00d64@o442880.ingest.sentry.io/5415687',
+  integrations: [
+    new Integrations.BrowserTracing({
+      tracingOrigins: ["localhost", "https://yourserver.io/api"],
+      routingInstrumentation: Sentry.routingInstrumentation,
+    }),
+  ],
+  tracesSampleRate: 1.0,
+});
+
+enableProdMode();
+platformBrowserDynamic()
+  .bootstrapModule(AppModule)
+  .then(success => console.log(`Bootstrap success`))
+  .catch(err => console.error(err));
+//below is original bro
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'my-app';
+}
